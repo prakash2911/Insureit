aeb6f43a6696813a3677f79b972718447b07fc93
Vinay-S-R <vinaydanlp@gmail.com>
2022-07-09 21:51:43 +0530

    Added code for ML Integration with chatbot

--- EXL_Project/app.py
+++ EXL_Project/app.py
@@ -1,8 +1,11 @@
 from flask import Flask, redirect,request,render_template
 from pip import main
 from flask_mail import Mail, Message
+import pickle
+import numpy as np
 
 app = Flask(__name__, template_folder='template')
+model = pickle.load(open('D:\InsureIt\Insureit\EXL_Project\model.pkl', 'rb'))
 # configuration of mail
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
@@ -22,12 +25,32 @@ def send_mail():
         msg = Message(
             subject=service,
             sender='quizwebsiteadp@gmail.com',
-            recipients=['quizwebsiteadp@gmail.com']
+            recipients=[email]
         )
         # msg.add_recipient(email)
         msg.body=description
         mail.send(msg)
     return redirect('/contact')
+@app.route('/chatbot', methods=['get', 'post'])
+def predict():
+    age = request.form['Age']
+    sex = request.form['Sex']
+    bmi = request.form['BMI']
+    children = request.form['noofchildren']
+    smoker = request.form['Smoke']
+    if sex == 'Male':
+        sex = 1
+    else:
+        sex = 0
+    if smoker == 'Yes':
+        smoker = 1
+    else:
+        smoker = 0
+    final_features = [np.array((age, sex, bmi, children, smoker))]
+    prediction = model.predict(final_features)
+    output = round(prediction[0], 2)
+    print(prediction)
+    return render_template('index.html')
 @app.route('/')
 def home():
     return render_template('index.html')
